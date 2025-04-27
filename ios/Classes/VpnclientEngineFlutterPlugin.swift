4d186047ed636022cf01e24fd23f8606240e973f
Anton Dodonov <anton.v.dodonov@gmail.com>
2025-04-28 02:15:21 +0700

    ios

--- ios/Classes/VpnclientEngineFlutterPlugin.swift
+++ ios/Classes/VpnclientEngineFlutterPlugin.swift
@@ -1,21 +1,51 @@
 import Flutter
 import UIKit
-//import VPNclientTunnel
-//import VPNclientEngine
+import NetworkExtension
 
+/// Plugin class to handle VPN connections in the Flutter app.
 public class VpnclientEngineFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "vpnclient_engine_flutter", binaryMessenger: registrar.messenger())
-    let instance = VpnclientEngineFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    private var tunnelProvider: NETunnelProviderManager?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "vpnclient_engine_flutter", binaryMessenger: registrar.messenger())
+        let instance = VpnclientEngineFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "startVPN":
+            guard let args = call.arguments as? [String: Any],
+                  let config = args["config"] as? String else {
+                result(FlutterError(code: "INVALID_ARGUMENTS", message: "Missing or invalid config", details: nil))
+                return
+            }
+            startVPN(withConfig: config, result: result)
+        case "stopVPN":
+            stopVPN(result: result)
+        case "checkVPNStatus":
+            checkVPNStatus(result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    /// Starts the VPN connection.
+    private func startVPN(withConfig config: String, result: @escaping FlutterResult) {
+        // Implement the logic to start the VPN connection using PacketTunnelProvider
+        result(nil)
+    }
+    
+    /// Stops the VPN connection.
+    private func stopVPN(result: @escaping FlutterResult) {
+        // Implement the logic to stop the VPN connection
+        result(nil)
+    }
+    
+    /// Checks the current status of the VPN connection.
+    private func checkVPNStatus(result: @escaping FlutterResult) {
+        // Implement the logic to check the VPN connection status
+        result(nil)
+    }
 }
+
--- macos/Classes/VpnclientEngineFlutterPlugin.swift
+++ macos/Classes/VpnclientEngineFlutterPlugin.swift
@@ -1,20 +1,44 @@
 import Cocoa
 import FlutterMacOS
-// import VPNclientEngineIOS
+import NetworkExtension
 
+/// Plugin for managing VPN connections on macOS.
 public class VpnclientEngineFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "vpnclient_engine_flutter", binaryMessenger: registrar.messenger)
-    let instance = VpnclientEngineFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-    default:
-      result(FlutterMethodNotImplemented)
+    private var manager: NEVPNManager?
+    
+    /// Registers the plugin with the Flutter engine.
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "vpnclient_engine_flutter", binaryMessenger: registrar.messenger)
+        let instance = VpnclientEngineFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    /// Handles method calls from Flutter.
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "startVPN":
+            guard let config = call.arguments as? String else {
+                return result(FlutterError(code: "INVALID_ARGUMENTS", message: "Config is missing", details: nil))
+            }
+            startVPN(config: config, result: result)
+        case "stopVPN":
+            stopVPN(result: result)
+        case "checkVPNStatus":
+            checkVPNStatus(result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func startVPN(config: String, result: @escaping FlutterResult) {
+        // TODO: Implement startVPN logic
+    }
+    
+    private func stopVPN(result: @escaping FlutterResult) {
+        // TODO: Implement stopVPN logic
+    }
+    
+    private func checkVPNStatus(result: @escaping FlutterResult) {
+        // TODO: Implement checkVPNStatus logic
+    }
 }
